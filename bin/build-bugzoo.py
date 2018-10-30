1e5f2b6e762fe790eef8d5d876e9d4baa0e8f073
ChrisTimperley <christimperley@googlemail.com>
2018-10-30 16:40:19 -0400

    added a mostly functional bugzoo manifest builder

---
+++ bin/build-bugzoo.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python3
+import os
+import json
+
+import yaml
+
+DIR_FILE = os.path.dirname(__file__)
+DIR_ROOT = os.path.join(DIR_FILE, '..')
+FN_DEFECTS = os.path.join(DIR_ROOT, 'defect-classification.json')
+FN_BUGZOO = os.path.join(DIR_ROOT, 'introclass.bugzoo.yml')
+
+
+def build_bug(program, repo, revision):
+    # compute a name for the bug
+    rev_short = revision.split('_')[0].rjust(3, '0')
+    repo_short = repo[:6]
+    parts_name = [program, repo_short, rev_short]
+
+    # build the blueprint
+    name_image = "squareslab/introclass:{}"
+    name_image = name_image.format('-'.join(parts_name))
+    blueprint = {
+        'tag': name_image,
+        'arguments': {
+            'PROGRAM': program,
+            'REPO': repo,
+            'VERSION': rev_short
+        }
+    }
+
+    print("building bug: {}".format(name_image))
+
+    # determine the number of blackbox test cases
+    dir_bug = os.path.join(DIR_ROOT, program, repo, rev_short)
+    fn_blackbox = os.path.join(dir_bug, 'blackbox_test.sh')
+
+    num_passing = 0
+    num_failing = 0
+
+    # determine the build commands
+    cmd_build = 'gcc -o {0} {0}.c'.format(program)
+    cmd_clean = 'rm -f {}'.format(program)
+    cmd_ins = 'gcc --coverage -o {0} {0}.c'.format(program)
+
+    # build the bug description
+    name_bug = ':'.join(['introclass'] + parts_name)
+    bug = {
+        'name': name_bug,
+        'image': name_image,
+        'languages': ['c'],
+        'program': program,
+        'dataset': 'introclass',
+        'source-location': '/experiment',
+        'test-harness': {
+            'type': 'genprog',
+            'time-limit': 10,
+            'failing': num_failing,
+            'passing': num_passing
+        },
+        'compiler': {
+            'type': 'simple',
+            'time-limit': 10,
+            'command': cmd_build,
+            'command_clean': cmd_clean,
+            'command_with_instrumentation': cmd_ins
+        }
+    }
+
+    return bug, blueprint
+
+
+def main():
+    output = {
+        'version': '1.0',
+        'blueprints': [],
+        'bugs': []
+    }  # type: Dict[str, Any]
+
+    with open(FN_DEFECTS, 'r') as f:
+        defects = json.load(f)
+
+    for details_defect in defects:
+        program = details_defect['program']
+        repo = details_defect['repo']
+        revision = details_defect['revision']
+        bug, blueprint = build_bug(program, repo, revision)
+        output['bugs'].append(bug)
+        output['blueprints'].append(blueprint)
+
+    # write to YAML
+    with open(FN_BUGZOO, 'w') as f:
+        yaml.dump(output, f, default_flow_style=False)
+
+
+if __name__ == '__main__':
+    main()
